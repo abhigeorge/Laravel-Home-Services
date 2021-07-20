0159176a1b3242f20e51494accb8404d82af4493
abhigeorge <abhigeorgekalladayil@gmail.com>
2021-07-20 07:36:52 +0530

    Admin Delete Service Category

--- app/Http/Livewire/Admin/AdminServiceCategoryComponent.php
+++ app/Http/Livewire/Admin/AdminServiceCategoryComponent.php
@@ -9,9 +9,19 @@ use Livewire\WithPagination;
 class AdminServiceCategoryComponent extends Component
 {
     use WithPagination;
+
+    public function deleteServiceCategory($id)
+    {
+        $scategory = ServiceCategory::find($id);
+        if ($scategory->image) {
+            unlink('images/categories' . '/' . $scategory->image);
+        }
+        $scategory->delete();
+        session()->flash('message', 'Service Category has been deleted successfully!');
+    }
     public function render()
     {
         $scategories = ServiceCategory::paginate(10);
-        return view('livewire.admin.admin-service-category-component',['scategories'=>$scategories])->layout('layouts.base');
+        return view('livewire.admin.admin-service-category-component', ['scategories' => $scategories])->layout('layouts.base');
     }
 }
